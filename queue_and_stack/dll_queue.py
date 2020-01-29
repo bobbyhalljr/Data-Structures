99a8ba3858e78af337d4980d42231e044ad16294
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-01-29 10:56:37 -0500

    failing 1 test, testing length of list after push, stack and queues

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,22 +1,29 @@
 import sys
-sys.path.append('../doubly_linked_list')
-from doubly_linked_list import Doubly_linked_list
+# sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
 
+dll = DoublyLinkedList(node=None)
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        ###  they follow the FIFO
-        self.storage = Doubly_linked_list
+        
+        # it follows the FIFO method
+        self.storage = dll
 
     def enqueue(self, value):
-        self.storage.append(value)
+        if self.storage.length < 1:
+            return None
+        if self.storage.length == 1:
+            return 1
+        self.storage.add_to_head(value)
+        return self.storage.length
 
     def dequeue(self):
         if len(self.storage) > 1:
             return None
-        return self.storage.delete()
+        return self.storage.remove_from_head()
 
     def len(self):
         return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,31 @@
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import Doubly_linked_list
+from doubly_linked_list import DoublyLinkedList
+
+dll = DoublyLinkedList(node=None)
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = dll
 
     def push(self, value):
-        pass
+        if self.storage.length < 1:
+            return 0
+        elif self.storage > 1:
+            return self.size
+        self.storage.add_to_head(value)
+        return self.storage.length
 
     def pop(self):
-        pass
+        if self.storage > 1:
+            return len(self.storage)
+        self.storage.remove_from_tail()
+        return self.storage
 
     def len(self):
-        pass
+        return self.size
+
+    # def __str__(self):
+    #     return f"the dll -> storage: {self.storage}"
