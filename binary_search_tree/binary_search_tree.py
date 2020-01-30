7014bfb24953d07316b1423ed70288b28c41c0c8
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-01-30 14:33:22 -0500

    finished yesterdays problems

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -16,67 +16,58 @@ class BinarySearchTree:
         self.right = None
         self.root = Node(value)
 
-    # 
-
     # Insert the given value into the tree
     def insert(self, value):
-        # set curr_node pointer
-        curr_node = self.root
-        # if value is none, set value as root, return root
-        if value is None:
-            root = self.value
-            return root
-        # while loop / while current node
-        while curr_node:
-            # check if value is less than current node value
-            if value < curr_node:
-                # if current node left is none
-                if curr_node.left is None:
-                        # current node left equals Node()
-                        curr_node.left = Node(value)
-                        break
-                # else current node is current node left
-                else:
-                    curr_node = curr_node.left
-            # else 
+        # compare root node
+        if value < self.value:
+            # if lesser go left child
+            if not self.left:
+                self.left = BinarySearchTree(value)
             else:
-                # if current node right is none
-                if curr_node.right is None:
-                    # current node right equals Node()
-                    curr_node.right = Node(value)
-                    break
-                else:
-                    # current node is current node right
-                    curr_node = curr_node.right
+                # if something is already there, recurse
+                self.left.insert(value)
+        else:
+            # value is >= Node
+            # if greater go right child
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                # if something is already there, recurse
+                self.right.insert(value)
       
-      
-        # init new node = ...
-        # new_node = Node(value)
-        # root = self.value
-        # var - pointer - curr_node 
-        # curr_node = value
-        # while loop or recursion
-        # if root.value < new_node:
-        # 2x if less or greater than root 
-        # 2x if / else 
-
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        if target == self.value:
+        if self.value == target:
             return True
-        else: 
-            False
-
+        if target < self.value: 
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+    
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if not self.right:
+            return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
